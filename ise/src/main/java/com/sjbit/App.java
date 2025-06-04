a82e51c23bef01b93993b802efb1aa6aae9916bd
kimta <nehaltaehyunggupta@gmail.com>
2025-06-05 01:47:11 +0530

    azure pipeline example

---
+++ ise/src/main/java/com/sjbit/App.java
@@ -0,0 +1,13 @@
+package com.sjbit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
